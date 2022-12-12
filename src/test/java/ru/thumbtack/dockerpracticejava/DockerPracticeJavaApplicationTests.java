3c9ad6c08185c138e07d20a5df6858bbf83881bd
krause_a_v@mail.ru <Fktrctqr3377>
2022-12-12 14:56:46 +0600

    Krauze_Aleksei_1.3

--- src/test/java/ru/thumbtack/dockerpracticejava/DockerPracticeJavaApplicationTests.java
+++ src/test/java/ru/thumbtack/dockerpracticejava/DockerPracticeJavaApplicationTests.java
@@ -3,11 +3,11 @@ package ru.thumbtack.dockerpracticejava;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class DockerPracticeJavaApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@SpringBootTest
+class DockerPracticeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
